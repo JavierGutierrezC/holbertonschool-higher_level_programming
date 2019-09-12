7bc98b27e8d8397a24e8e8035720afb32f49d09e
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-12 20:01:55 +0000

    changed an if for elif

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
 
     if(len(sys.argv) - 1) == 0:
         print("{:d} arguments.".format(len(sys.argv) - 1))
-    if(len(sys.argv) - 1) == 1:
+    elif(len(sys.argv) - 1) == 1:
         print("{:d} argument:".format(len(sys.argv) - 1))
         print("{:d}: {:s}".format(1, sys.argv[1]))
     else:
