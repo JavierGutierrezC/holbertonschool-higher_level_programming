785e833f479d6a079805872c5fc27ce17b661014
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-12 19:54:07 +0000

    add if condition

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+
+    if(len(sys.argv) - 1) == 0:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    if(len(sys.argv) - 1) == 1:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+        print("{:d}: {:s}".format(1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
