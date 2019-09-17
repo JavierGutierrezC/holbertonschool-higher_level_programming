be5a821828983883d15e052e700a85152fd7e561
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-17 15:35:03 +0000

    [Fix]return indentation

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx < 0:
+        print(None)
+    elif idx > len(my_list):
+        print(None)
+    else:
+        for x in range(len(my_list)):
+            if x == idx:
+                return(my_list[x])
