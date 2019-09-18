88f69dc78989dd8dc2646be73a20536a42a54723
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-18 00:45:42 +0000

    [Fix]eliminated for

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         print(None)
-    elif idx >= len(my_list):
+    elif idx > len(my_list):
         print(None)
     else:
-        for x in range(len(my_list)):
-            if x == idx:
-                return(my_list[x])
+        return(my_list[idx])
