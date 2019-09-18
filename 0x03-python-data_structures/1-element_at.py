53aa15e079f1e3d3b91e69bc073de1fcdbfe2085
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-18 00:47:15 +0000

    [Fix]add =

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     if idx < 0:
         print(None)
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         print(None)
     else:
         return(my_list[idx])
