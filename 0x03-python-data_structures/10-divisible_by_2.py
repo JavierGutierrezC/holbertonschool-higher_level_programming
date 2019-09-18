6b89df9df94f03765cb1f60ebddee9315657fd13
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-18 01:53:37 +0000

    [Fix]copylis in for

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,7 +1,7 @@
 #!/ust/bin/python3
 def divisible_by_2(my_list=[]):
     copylis = my_list[:]
-    for x in range(len(my_list)):
+    for x in range(len(copylis)):
         if (copylis[x] % 2 == 0):
             copylis[x] = True
         else:
