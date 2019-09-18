006edb304d97bef87a606cbb68badcd6a0098269
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-18 00:50:19 +0000

    [Fix]changed print for returns

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    x = len(my_list)
     if idx < 0:
-        print(None)
-    elif idx >= len(my_list):
-        print(None)
+        return(None)
+    elif idx >= x:
+        return(None)
     else:
         return(my_list[idx])
