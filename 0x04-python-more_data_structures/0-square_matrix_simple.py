b668323508e0da40284b254dd37b4a1bb72ace73
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-09-19 18:55:13 +0000

    [Fix]add spaces

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,5 +2,5 @@
 def square_matrix_simple(matrix=[]):
     copy_matrix = []
     for x in matrix:
-        copy_matrix.append(list(map(lambda y : y**2, x)))
+        copy_matrix.append(list(map(lambda y: y**2, x)))
     return(copy_matrix)
