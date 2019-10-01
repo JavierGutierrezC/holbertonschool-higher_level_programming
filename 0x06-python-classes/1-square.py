f34ac1b2d5fdf0b36c991b62e7c3267f19cace9d
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-10-01 23:33:49 +0000

    [fix]Added space after comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,9 +2,10 @@
 
 """Write a class Square that defines a square"""
 
+
 class Square:
 
     """Initiate class size. size is a private instace"""
 
-    def __init__ (self, size):
+    def __init__(self, size):
         self.__size = size
