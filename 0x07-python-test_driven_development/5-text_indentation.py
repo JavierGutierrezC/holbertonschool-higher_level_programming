07ad25537c6d5229c37865ce32655a39461fd398
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-10-05 16:22:36 +0000

    [Fix]Spaces after coma

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -17,6 +17,6 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    tc = text.replace(". ",".").replace("? ","?").replace(": ",":")
+    tc = text.replace(". ", ".").replace("? ", "?").replace(": ", ":")
     nt = tc.replace(".", ".\n\n").replace("?", "?\n\n").replace(":", ":\n\n")
     print(nt, end="")
