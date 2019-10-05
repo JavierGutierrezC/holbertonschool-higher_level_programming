c743dccf693b661d522d48de148ff5072a164809
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-10-05 19:45:51 +0000

    [Fix]Added test to def

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,25 +7,25 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
 
-    def max_at_end(self):
+    def test_max_at_end(self):
         self.assertEqual(max_integer([2,4,6,8]), 8)
 
-    def max_at_start(self):
+    def test_max_at_start(self):
         self.assertEqual(max_integer([8,4,6,3]), 8)
 
-    def max_at_middle(self):
+    def test_max_at_middle(self):
         self.assertEqual(max_integer([2,4,9,8,6]), 9)
 
-    def negative_number_in_list(self):
+    def test_negative_number_in_list(self):
         self.assertEqual(max_integer([2,-7,6,8]), 8)
 
-    def only_neg_numbers(self):
+    def test_only_neg_numbers(self):
         self.assertEqual(max_integer([-2,-4,-6,-8]), -2)
 
-    def only_one_element(self):
+    def test_only_one_element(self):
         self.assertEqual(max_integer([2]), 2)
 
-    def empty_list(self):
+    def test_empty_list(self):
         self.assertEqual(max_integer([]), None)
 
 if __name__ == '__main__':
