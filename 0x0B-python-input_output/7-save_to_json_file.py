2d2262dab5089652a796024ebbeed00231e06b33
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-10-16 23:56:48 +0000

    [Fix]pep8

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -2,6 +2,7 @@
 
 import json
 
+
 def save_to_json_file(my_obj, filename):
-    with open(filename, mode="w", encoding= "UTF-8") as file_7:
+    with open(filename, mode="w", encoding="UTF-8") as file_7:
         json.dump(my_obj, file_7)
--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -2,6 +2,7 @@
 
 import json
 
+
 def load_from_json_file(filename):
     with open(filename, "r", encoding="UTF-8") as file_8:
         return (json.load(file_8))
