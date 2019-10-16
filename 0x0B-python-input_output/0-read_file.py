dd381cde87f29aeeabe1aa280c5dba9ff08166a6
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-10-16 23:23:33 +0000

    [Fix]Nothing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    """comment"""
-    with open(filename, encoding="UTF-8") as fl:
-        read_fl = fl.read()
-        print(read_fl, end="")
+    with open(filename, encoding="UTF-8") as files_1:
+        print(files_1.read(), end="")
