571190b68c24c832c3ea52f0782c44ff99459b46
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-10-16 23:31:06 +0000

    none

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, encoding="UTF-8") as files_1:
-        print(files_1.read(), end="")
+    with open(filename, encoding="UTF-8") as files_2:
+        print(files_2.read(), end="")
