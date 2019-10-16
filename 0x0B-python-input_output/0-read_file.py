39f17ee7f1596aa6583826cfec7bd7a7d9a7451d
JavierGutierrezC <javier.gutierrez.cruz@gmail.com>
2019-10-16 23:12:20 +0000

    [Fix]Nothing

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, encoding="UTF-8") as file_1:
-        print(file_1.read(), end="")
+    with open(filename, encoding="UTF-8") as files_1:
+        print(files_1.read(), end="")
