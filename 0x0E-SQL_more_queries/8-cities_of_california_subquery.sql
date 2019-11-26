40ff0e16da8eb88d34ab3956dac8db51297a32af
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-11-26 21:06:54 +0000

    state.id

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,5 +2,5 @@
 --
 SELECT cities.id, cities.name FROM cities 
 WHERE state_id =(
-SELECT state_id FROM states WHERE name = "California")
+SELECT states.id FROM states WHERE name = "California")
 ORDER by id ASC;
