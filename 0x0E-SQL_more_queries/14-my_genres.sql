e6c6bd69dada41ccde55ebd45b3e5b6d9c82651c
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-11-28 00:46:50 +0000

    my_genre

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,10 @@
+-- My genres
+-- 
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_genres.id = tv_shows_genres.genre.id
+JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
