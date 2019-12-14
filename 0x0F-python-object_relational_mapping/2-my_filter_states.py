d2c1d6348b1968687336eeb64b615515b4b06d8b
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-12-14 21:03:54 +0000

    [Fix],format out of double quotes

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    javi_db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306)
+    cur = javi_db.cursor()
+
+    cur.execute("SELECT * FROM states WHERE name= '{:s}'".format(argv[4]))
+    for row in cur.fetchall():
+        print(row)
+    javi_db.close()
+    cur.close()
