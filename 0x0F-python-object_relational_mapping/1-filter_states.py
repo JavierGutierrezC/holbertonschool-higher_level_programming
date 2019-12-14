e25b3348b2f3356ccf5c650c4c7d73a543883d04
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-12-14 19:49:08 +0000

    [Fix]added order by

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,8 +12,7 @@ if __name__ == '__main__':
         port=3306)
     cur = javi_db.cursor()
 
-    cur.execute("SELECT id, name FROM states WHERE name REGEXP '^[N]'")
-    """rows = cur.fetchall()"""
+    cur.execute("SELECT * FROM states ORDER BY id ASC WHERE name REGEXP '^[N]'")
     for row in cur.fetchall():
         print(row)
     javi_db.close()
