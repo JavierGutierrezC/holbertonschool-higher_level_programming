40806ac5a70aa44c81f8382eedfa6d725cb45238
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-12-14 19:37:09 +0000

    [Feature] filter by states

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    javi_db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306)
+    cur = javi_db.cursor()
+
+    cur.execute("SELECT id, name FROM states WHERE name REGEXP '^[N]'")
+    """rows = cur.fetchall()"""
+    for row in cur.fetchall():
+        print(row)
+    javi_db.close()
+    cur.close()
