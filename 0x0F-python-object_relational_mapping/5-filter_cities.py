3ddd287980c6ea0a058eecccd3479f182dd4f6c6
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2019-12-15 14:01:35 +0000

    [Fix]changed a = a + b for +=

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    javi_db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        password=argv[2],
+        database=argv[3],
+        port=3306)
+    cur = javi_db.cursor()
+
+    cur.execute("SELECT cities.name FROM states \
+    JOIN cities ON cities.state_id = states.id WHERE states.name = %s\
+    ORDER by cities.id",[argv[4]])
+    new = []
+    for row in cur.fetchall():
+        new += row
+    print(', '.join(new))
+    javi_db.close()
+    cur.close()
