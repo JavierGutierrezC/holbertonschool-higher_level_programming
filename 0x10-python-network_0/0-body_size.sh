347737690d86747701d92893c2e030f611eeed4e
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-07 20:47:42 +0000

    [Fix]add - to f2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -sD -"$1" | grep "Content-Lenght" | cut -d " " f2
+curl -sD -"$1" | grep "Content-Lenght" | cut -d " " -f2
