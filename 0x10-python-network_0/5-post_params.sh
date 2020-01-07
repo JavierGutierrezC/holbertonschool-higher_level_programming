c22f5016f8c2b40ebb89f8ac10f54ae2b46ac402
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-07 17:30:31 +0000

    [Fix]changed -s-d for -sd

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sd "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+
