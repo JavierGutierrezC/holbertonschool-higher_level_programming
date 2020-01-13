91401dfc3928888d4dae6d6cd13c8e64254bedc7
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-13 20:24:42 +0000

    [Fix]erased a comment

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -7,7 +7,6 @@ from sys import argv
 
 if __name__ == "__main__":
     with urllib.request.urlopen(argv[1]) as response:
-        #dir(response)
         urlreq = response.info()
         for key, value in urlreq.items():
             if key == "X-Request-Id":
