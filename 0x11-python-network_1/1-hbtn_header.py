679b4d94ad04cc070dc68d63a3aed6f8430f11e4
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-13 20:04:47 +0000

    [Fix]changed peek for info

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Takes a URL and sends a request ti the url
+"""
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    with urllib.request.urlopen(argv[1]) as response:
+        #dir(response)
+        urlreq = response.info()
+        for key, value in urlreq.items():
+            if key == "X-Request-Id":
+                print(value)
