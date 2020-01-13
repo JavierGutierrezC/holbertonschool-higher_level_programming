bb2db9ed478d872b4fa1f1c5a2483c2e43ff249e
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-13 21:45:05 +0000

    [Fix]add decode(UTF-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response
+"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    mail = {"email": argv[2]}
+    data = urllib.parse.urlencode(mail)
+    data = data.encode("utf-8")
+    req = urllib.request.Request(url,data)
+    with urllib.request.urlopen(req) as response:
+        urlreq = response.read()
+        print(urlreq.decode("UTF-8"))
