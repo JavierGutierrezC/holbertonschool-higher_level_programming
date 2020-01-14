60e8679bf1c8746c95abc6d6c35b246495a1d162
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-14 00:25:44 +0000

    [Fix]add headers

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Takes a URL and sends a request ti the url
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    value =r.headers.get("X-Request-Id")
+    print(value)
