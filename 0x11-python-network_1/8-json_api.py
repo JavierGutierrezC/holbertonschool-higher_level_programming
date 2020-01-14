176453fb01532cee3dc7fe265933298c1914d96b
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-14 03:55:04 +0000

    [Fix]condition

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    if len(argv) is not 2 or not argv[1].isalpha():
+        # print(len(argv))
+        # print(argv[0])
+        # print(argv[2])
+        print("No result")
+    else:
+        url = "http://0.0.0.0:5000/search_user"
+        q = argv[1]
+        r = requests.post(url, data={"q": q})
+        # print(r.text)
+        # print(r.json())
+        jason = r.json()
+        try:
+            print("[{}] {}".format(jason.get("id"), jason.get("name")))
+        except Exception:
+            print("Not a valid JSON")
