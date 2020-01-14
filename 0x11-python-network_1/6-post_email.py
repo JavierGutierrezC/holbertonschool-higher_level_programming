8a6eee423a10e84c77281aa7a59769c06db35b97
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-01-14 00:31:09 +0000

    [Fix]add .txt

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL and an email, sends a POST request to the passed URL
+"""
+import urllib.request
+import urllib.parse
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    mail = {"email": argv[2]}
+    r = requests.post(url, mail)
+    print(r.text)
