4fdcd04fce97d6b04c717fba6b0c86bdc0ebb373
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-02-11 19:53:05 +0000

    [Fix]semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 
-const fs = require('fs'), file = process.argv[2], cont = process.argv[3];
-fs.writeFile(file, cont, 'utf8', (err,data) => {
+const fs = require('fs');
+const file = process.argv[2];
+const cont = process.argv[3];
+fs.writeFile(file, cont, 'utf8', err => {
   if (err) {
-    console.error(err)
-    return
+    console.error(err);
   }
 });
