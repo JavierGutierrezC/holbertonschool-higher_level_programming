3ab67254f982e644621b42f7570006ee53a0290f
Javier.Gutierrez.C <javier.gutierrez.cruz@gmail.com>
2020-02-13 01:54:11 +0000

    change code

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  let x = 0;
+  for (let y = 0; y <= list.length; y++) {
+    if (list[y] === searchElement) {
+      x++;
+    }
+  }
+  return (x);
+};
